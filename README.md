e4124a4bbb4c0951d7b93ba6d10c7133d0949a2f
Harry Moreno <morenoh149@gmail.com>
2019-01-15 16:13:32 -0500

    USA dataset major cities

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ software for scraping some website
 ## Running
 Pass in the site as an environment variable. Do not add it to version control
 to keep it a secret
-1. `HOSTNAME=<site url> scrapy crawl nyc -o questions.csv`
+1. `HOSTNAME=foo.com scrapy crawl nyc -o questions.csv`
+read /site_scraper/spiders/city_spider.py to see how the hostname is used
+
 
 ## Resources
 * https://doc.scrapy.org/en/1.5/intro/tutorial.html
---
+++ site_scraper/spiders/austin_spider.py
@@ -0,0 +1,33 @@
+import scrapy
+import os
+hostname = os.environ['HOSTNAME']
+
+
+class NYCSpider(scrapy.Spider):
+    name = "austin"
+
+    def start_requests(self):
+        urls = [
+            'https://{}/Interview/austin-data-scientist-interview-questions-SRCH_IL.0,6_IM60_KO7,21.htm'.format(hostname),
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for row in response.css(".row"):
+            if row.css('.questionText::text').extract_first():
+                yield {
+                    'author': row.css('.authorInfo > a::text').extract_first(),
+                    'question': row.css('.questionText::text').extract_first(),
+                }
+        next_page = response.css('.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+'''
+        page = response.url.split("/")[-1]
+        filename = 'questions-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+'''
